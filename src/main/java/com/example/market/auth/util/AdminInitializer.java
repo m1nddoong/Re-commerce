dd86e9fad370af4ee31ebcba0bf50309e06d938a
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-07-31 21:40:53 +0900

    feat/admin : 더미 데이터 ApplicationRunner 수정

--- src/main/java/com/example/market/auth/util/AdminInitializer.java
+++ src/main/java/com/example/market/auth/util/AdminInitializer.java
@@ -23,13 +23,13 @@ public class AdminInitializer implements ApplicationRunner {
                 .uuid(UUID.randomUUID())
                 .email("admin123@naver.com")
                 .password(passwordEncoder.encode("1111"))
-                .username("유재석")
+                .username("송승현")
                 .nickname("관리자")
                 .age(26)
-                .phone("010-9292-5757")
+                .phone("010-3637-6533")
                 .profileImg(null)
                 .businessNum("605-46-5452")
-                .businessApply(true)
+                .businessApply(false)
                 .authorities("ROLE_ACTIVE,ROLE_OWNER,ROLE_ADMIN")
                 .build());
 
@@ -38,14 +38,44 @@ public class AdminInitializer implements ApplicationRunner {
                 .uuid(UUID.randomUUID())
                 .email("owner123@naver.com")
                 .password(passwordEncoder.encode("2222"))
-                .username("박명수")
+                .username("유재석")
                 .nickname("사업자 사용자")
                 .age(40)
                 .phone("010-7878-1313")
                 .profileImg(null)
                 .businessNum("603-46-2424")
-                .businessApply(true)
+                .businessApply(false)
                 .authorities("ROLE_ACTIVE,ROLE_OWNER")
+                .build());
+
+        // 일반 사용자 1
+        userRepository.save(User.builder()
+                .uuid(UUID.randomUUID())
+                .email("user1@naver.com")
+                .password(passwordEncoder.encode("3333"))
+                .username("박명수")
+                .nickname("일반 사용자")
+                .age(37)
+                .phone("010-3535-0909")
+                .profileImg(null)
+                .businessNum("623-46-2424")
+                .businessApply(true)
+                .authorities("ROLE_ACTIVE")
+                .build());
+
+        // 일반 사용자 2
+        userRepository.save(User.builder()
+                .uuid(UUID.randomUUID())
+                .email("user1@naver.com")
+                .password(passwordEncoder.encode("4444"))
+                .username("정준하")
+                .nickname("일반 사용자")
+                .age(37)
+                .phone("010-3535-0909")
+                .profileImg(null)
+                .businessNum(null)
+                .businessApply(true)
+                .authorities("ROLE_ACTIVE")
                 .build());
     }
 }
