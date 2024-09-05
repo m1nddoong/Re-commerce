ff7d4a0ef5913620c92d6f8db121337efea80ed8
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-09-05 16:24:57 +0900

    fix(user): PK tsid 변경

--- src/main/java/com/example/market/domain/auth/entity/User.java
+++ src/main/java/com/example/market/domain/auth/entity/User.java
@@ -8,6 +8,7 @@ import com.example.market.domain.used_trade.entity.TradeItem;
 import com.example.market.global.common.BaseEntity;
 import com.example.market.domain.shop.entity.Shop;
 import com.example.market.domain.auth.constant.SocialType;
+import io.hypersistence.utils.hibernate.id.Tsid;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -36,7 +37,8 @@ import lombok.experimental.SuperBuilder;
 @Table(name = "users")
 public class User extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Tsid
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private UUID uuid;
     @Column(nullable = false, unique = true)
